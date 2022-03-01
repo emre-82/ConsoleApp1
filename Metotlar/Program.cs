c098e663a58664aabe028a33383c99e802180345
emre-82 <yunus-rusen_282834@outlook.com>
2022-03-01 16:28:28 +0300

    yyyy

---
+++ Metotlar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+
+        public void ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete eklendi : " + urun.Adi );
+        }
+
+
+        
+    }
+}
